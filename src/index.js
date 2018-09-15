c9ae1a3eae9c345488bcc7961c61d96a408fac50
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-09-15 16:25:08 +0700

    Setup store and prevent it to collected by test coverage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "normalize.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { store, persistor } from "./store/store";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
---
+++ src/store/store.js
@@ -0,0 +1,32 @@
+import { createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import projects from "../store/reducers/projects";
+import slides from "../store/reducers/slides";
+import contents from "../store/reducers/contents";
+import activeProjectID from "../store/reducers/activeProjectID";
+import activeSlideID from "../store/reducers/activeSlideID";
+import activeContentID from "../store/reducers/activeContentID";
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const reducers = combineReducers({
+  projects,
+  slides,
+  contents,
+  activeProjectID,
+  activeSlideID,
+  activeContentID
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(
+  persistedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export const persistor = persistStore(store);
