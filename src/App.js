10308f6b61a899c2503234c07b0a8a795b1e1d46
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-09-16 23:55:33 +0700

    Add Editor component in root App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
+import { connect } from "react-redux";
 
-import Home from "./components/container/Home";
+import Header from "./components/container/Home";
+import Editor from "./components/container/Editor";
 
 const Container = styled.div`
   width: 100%;
   min-height: 100vh;
 `;
 
-const App = () => (
-  <Container>
-    <Home />
-  </Container>
+export const App = ({ activeProjectID }) => (
+  <Container>{activeProjectID ? <Editor /> : <Header />}</Container>
 );
 
-export default App;
+App.propTypes = {
+  activeProjectID: PropTypes.string
+};
+
+const mapStateToProps = ({ activeProjectID }) => ({ activeProjectID });
+
+export default connect(mapStateToProps)(App);
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
+import configureMockStore from "redux-mock-store";
 
-import App from "../App";
+import Connected, { App } from "../App";
 
 jest.mock("../components/container/Home", () => "Home");
+jest.mock("../components/container/Editor", () => "Editor");
 
 describe("App", () => {
+  const mockStore = configureMockStore();
+
   const setup = propOverrides => {
     const props = {
       ...propOverrides
@@ -19,9 +23,44 @@ describe("App", () => {
     };
   };
 
+  const setupConnected = storeOverrides => {
+    const store = mockStore({
+      activeProjectID: null,
+      ...storeOverrides
+    });
+    const wrapper = shallow(<Connected store={store} />);
+
+    return {
+      wrapper,
+      store
+    };
+  };
+
   it("should renders default", () => {
     const { wrapper } = setup();
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should renders as connected", () => {
+    const { wrapper } = setupConnected();
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should renders with active project", () => {
+    const { wrapper } = setup({
+      activeProjectID: "project-1"
+    });
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should map new to props", () => {
+    const { wrapper } = setupConnected();
+
+    expect(wrapper.props()).toMatchObject({
+      activeProjectID: null
+    });
   });
 });
